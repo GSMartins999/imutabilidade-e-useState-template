66ebfd666ffe90db9eadd67643916e78ee65e772
Giovanni Souza <GSMartins999>
2023-08-20 20:01:08 -0300

    imutabilidadeeusestate-feito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import { Garagem } from './Componentes/Garagem'
 import { GlobalStyled } from './GlobalStyled'
+import { useState } from 'react'
 
 export default function App() {
+  //criando um estado chamado nome
+  const [nome, setNome] = useState("Giovanni")
+  //criando um estado automovel para infos do carro
+  const [automovel, setAutomovel] = useState({
+    modelo:"Corsa",
+    cor: "Branco",
+    ano: 2020,
+    flex: true,
+    adicionadoPor: "Giovanni",
+  },
+  )
+
+  const carro1 ={ 
+    modelo:"Fusca",
+    cor:"Azul",
+    ano: 2005,
+    flex: false,
+    adicionadoPor: "Giovanni",
+  }
+  
+  const carro2 ={ 
+    modelo:"Toro",
+    cor:"Preta",
+    ano: 2020,
+    flex: true,
+    adicionadoPor: "Giovanni",
+
+  }
+
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem nome={nome} automovel={automovel} setNome={setNome} setAutomovel={setAutomovel} carro1={carro1} carro2 ={carro2}/>
     </div>
   )
 }
--- src/Componentes/Garagem/index.js
+++ src/Componentes/Garagem/index.js
@@ -1,19 +1,26 @@
 import { Carro } from '../Carro'
 import { Botao, Estacionamento, GaragemContainer } from './styles'
 
-export function Garagem({ nome }) {
+export function Garagem({ nome, automovel, setNome, setAutomovel,carro1,carro2 }) {
+ 
+  function passarPraCima(){
+    setAutomovel(carro1)
+    setNome("Lin") 
+  }
+
   return (
     <GaragemContainer>
-      <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
+      <h1>Garagem do {nome}</h1>
+      <Botao onClick={passarPraCima}>Outro usuário tem: </Botao>
+  
 
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          modelo={automovel.modelo}
+          cor={automovel.cor}
+          ano={automovel.ano}
+          adicionadoPor={automovel.adicionadoPor}
+          flex={automovel.flex}
         />
       </Estacionamento>
     </GaragemContainer>
